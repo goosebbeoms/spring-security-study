90a9665d1c048cf3883ef185d23cff8f4518d2c0
goosebbeoms <seo980916@gmail.com>
2025-05-02 15:09:05 +0900

    회원 중복 검증 로직 추가

--- TestSecurity/src/main/java/com/example/testsecurity/service/JoinService.java
+++ TestSecurity/src/main/java/com/example/testsecurity/service/JoinService.java
@@ -5,7 +5,6 @@ import com.example.testsecurity.entity.UserEntity;
 import com.example.testsecurity.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,9 +17,13 @@ public class JoinService {
 
     public void joinProcess(JoinDTO joinDTO) {
         // db에 이미 동일한 username을 가진 회원이 존재하는지?
+        boolean isExist = userRepository.existsByUsername(joinDTO.getUsername());
+        if (isExist) {
+            System.out.println("중복 회원");
+            return;
+        }
 
         UserEntity data = new UserEntity();
-
         data.setUsername(joinDTO.getUsername());
         data.setPassword(bCryptPasswordEncoder.encode(joinDTO.getPassword()));
         data.setRole("ROLE_USER");
