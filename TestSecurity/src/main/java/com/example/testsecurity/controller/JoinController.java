3605aad944a87cf048bd3b6ecffa82de13a2404d
goosebbeoms <seo980916@gmail.com>
2025-05-02 14:49:06 +0900

    MySQL DB 연결, 회원 가입 로직(DTO, Controller, Entity, Repository, Service)

---
+++ TestSecurity/src/main/java/com/example/testsecurity/controller/JoinController.java
@@ -0,0 +1,29 @@
+package com.example.testsecurity.controller;
+
+import com.example.testsecurity.dto.JoinDTO;
+import com.example.testsecurity.service.JoinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class JoinController {
+
+    @Autowired
+    private JoinService joinService;
+
+    @GetMapping("/join")
+    public String joinPage() {
+        return "join";
+    }
+
+    @PostMapping("/join-proc")
+    public String joinProcess(JoinDTO joinDTO) {
+        System.out.println(joinDTO.getUsername());
+
+        joinService.joinProcess(joinDTO);
+
+        return "redirect:/login";
+    }
+}
